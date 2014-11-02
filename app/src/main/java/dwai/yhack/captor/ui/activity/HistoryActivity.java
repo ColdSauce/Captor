899f6da6037cff74ad92e895540c4b0ae14cd276
Stefan <soccerfanatic1996@gmail.com>
2014-11-02 01:30:14 -0500

    added post requests

--- app/src/main/java/dwai/yhack/captor/ui/activity/HistoryActivity.java
+++ app/src/main/java/dwai/yhack/captor/ui/activity/HistoryActivity.java
@@ -19,12 +19,23 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.List;
 
 import dwai.yhack.captor.R;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -51,18 +62,18 @@ public class HistoryActivity extends Activity {
 
 
 
-        new JSONParse().execute();
-
+//        new JSONParse().execute();
+        new PostStuff().execute("http://captor.thupukair.com");
     }
 
     private String getPhoneNumber() {
-        TelephonyManager tMgr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        TelephonyManager tMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         String mPhoneNumber = tMgr.getLine1Number();
 
         return mPhoneNumber;
     }
 
-    public String getMy10DigitPhoneNumber(){
+    public String getMy10DigitPhoneNumber() {
         String s = getPhoneNumber();
         return s != null && s.length() > 1 ? s.substring(1) : null;
     }
@@ -92,15 +103,15 @@ public class HistoryActivity extends Activity {
             List<OneItem> items = new ArrayList<OneItem>();
             Bitmap icon = BitmapFactory.decodeResource(getResources(),
                     R.drawable.kitten);
-            final LinearLayout slideView = ((LinearLayout)findViewById(R.id.slideView));
+            final LinearLayout slideView = ((LinearLayout) findViewById(R.id.slideView));
             try {
-                for(int i = 0; i < json.length(); i++) {
-                    OneItem o = new OneItem(icon,json.getJSONObject(i).getString("text") + "..." + i, new DatePosted().setYear(2).setMonth(3).setDay(23).setHour(12).setMinute(13).setSecond(24));
+                for (int i = 0; i < json.length(); i++) {
+                    OneItem o = new OneItem(icon, json.getJSONObject(i).getString("text") + "..." + i, new DatePosted().setYear(2).setMonth(3).setDay(23).setHour(12).setMinute(13).setSecond(24));
                     items.add(o);
                 }
                 Collections.sort(items);
-                ListView lv = ((ListView)findViewById(R.id.rootListView));
-                Log.d("Joe", "kkkkkkkkk" +items.toString());
+                ListView lv = ((ListView) findViewById(R.id.rootListView));
+                Log.d("Joe", "kkkkkkkkk" + items.toString());
                 lv.setAdapter(new EasyAdapter<OneItem>(getApplicationContext(), HistoryViewHolder.class, items));
             } catch (Exception e) {
                 Log.d("Joe", "hello");
@@ -130,27 +141,41 @@ public class HistoryActivity extends Activity {
     }
 
 
+    private class PostStuff extends AsyncTask<String, Void, Void> {
+        @Override
+        protected Void doInBackground(String... strings) {
 
-    public static final void setAppFont(ViewGroup mContainer, Typeface mFont, boolean reflect)
-    {
+            // Create a new HttpClient and Post Header
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(URL + "/api/data?username=" + getMy10DigitPhoneNumber() + "&string=" + "stuff");
+
+            try{
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+
+
+        }
+
+    }
+
+
+    public static final void setAppFont(ViewGroup mContainer, Typeface mFont, boolean reflect) {
         if (mContainer == null || mFont == null) return;
         final int mCount = mContainer.getChildCount();
         // Loop through all of the children.
-        for (int i = 0; i < mCount; ++i)
-        {
+        for (int i = 0; i < mCount; ++i) {
             final View mChild = mContainer.getChildAt(i);
-            if (mChild instanceof TextView)
-            {
+            if (mChild instanceof TextView) {
                 // Set the font if it is a TextView.
                 ((TextView) mChild).setTypeface(mFont);
-            }
-            else if (mChild instanceof ViewGroup)
-            {
+            } else if (mChild instanceof ViewGroup) {
                 // Recursively attempt another ViewGroup.
-                setAppFont((ViewGroup) mChild, mFont,true);
-            }
-            else if (reflect)
-            {
+                setAppFont((ViewGroup) mChild, mFont, true);
+            } else if (reflect) {
                 try {
                     Method mSetTypeface = mChild.getClass().getMethod("setTypeface", Typeface.class);
                     mSetTypeface.invoke(mChild, mFont);
--- app/src/main/java/dwai/yhack/captor/ui/activity/MyActivity.java
+++ app/src/main/java/dwai/yhack/captor/ui/activity/MyActivity.java
@@ -15,7 +15,7 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        startActivity(new Intent(MyActivity.this, HistoryActivity.class));
+        startActivity(new Intent(MyActivity.this, ArActivity.class));
 
     }
 }
