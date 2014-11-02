e9451d615c0a085cdb66cb10a378e5741a30dbad
Stefan <soccerfanatic1996@gmail.com>
2014-11-01 21:32:41 -0400

    added some designs

--- app/src/main/java/dwai/yhack/captor/ui/activity/HistoryActivity.java
+++ app/src/main/java/dwai/yhack/captor/ui/activity/HistoryActivity.java
@@ -1,9 +1,16 @@
 package dwai.yhack.captor.ui.activity;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.lang.reflect.Method;
+
 import dwai.yhack.captor.R;
 
 public class HistoryActivity extends Activity {
@@ -12,6 +19,12 @@ public class HistoryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        final Typeface mFont = Typeface.createFromAsset(getAssets(),
+                "fonts/Roboto-Medium.ttf");
+        final ViewGroup mContainer = (ViewGroup) findViewById(
+                android.R.id.content).getRootView();
+        HistoryActivity.setAppFont(mContainer, mFont, true);
     }
 
 
@@ -33,4 +46,37 @@ public class HistoryActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    public static final void setAppFont(ViewGroup mContainer, Typeface mFont, boolean reflect)
+    {
+        if (mContainer == null || mFont == null) return;
+        final int mCount = mContainer.getChildCount();
+// Loop through all of the children.
+        for (int i = 0; i < mCount; ++i)
+        {
+            final View mChild = mContainer.getChildAt(i);
+            if (mChild instanceof TextView)
+            {
+// Set the font if it is a TextView.
+                ((TextView) mChild).setTypeface(mFont);
+            }
+            else if (mChild instanceof ViewGroup)
+            {
+// Recursively attempt another ViewGroup.
+                setAppFont((ViewGroup) mChild, mFont,true);
+            }
+            else if (reflect)
+            {
+                try {
+                    Method mSetTypeface = mChild.getClass().getMethod("setTypeface", Typeface.class);
+                    mSetTypeface.invoke(mChild, mFont);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/dwai/yhack/captor/ui/activity/MyActivity.java
+++ app/src/main/java/dwai/yhack/captor/ui/activity/MyActivity.java
@@ -13,7 +13,7 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        startActivity(new Intent(MyActivity.this, ArActivity.class));
+        startActivity(new Intent(MyActivity.this, HistoryActivity.class));
 
     }
 }
