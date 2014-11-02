891f7300547ae876258b9af503024c6ceed5f1aa
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-11-02 02:02:29 -0500

    so dope

--- app/src/main/java/dwai/yhack/captor/ui/activity/MyActivity.java
+++ app/src/main/java/dwai/yhack/captor/ui/activity/MyActivity.java
@@ -1,8 +1,10 @@
 package dwai.yhack.captor.ui.activity;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import org.json.JSONObject;
 
@@ -15,7 +17,22 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        startActivity(new Intent(MyActivity.this, ArActivity.class));
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
 
+    }
+
+    public void startCardBoard(View view) {
+        startActivity(new Intent(MyActivity.this, ArActivity.class));
+    }
+
+    public void startHistory(View view) {
+        startActivity(new Intent(MyActivity.this, HistoryActivity.class));
     }
 }
